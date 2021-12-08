8568e4e9898c65fad4edc8ee96d8b3d15f394d03
Carolina <carolina.albuquerque.cmp@gmail.com>
2021-12-08 22:31:18 +0000

    with the comments commented

--- Origem.cpp
+++ Origem.cpp
@@ -26,7 +26,7 @@ void bubbleSort(vector<char>& v){
 
 void setBag(ifstream& extractFile, vector<char>& bag, vector <char> rack)
 {
-	 Da primeira vez a função lê o ficheiro e põe as letras numa ordem aleatória
+	 //Da primeira vez a função lê o ficheiro e põe as letras numa ordem aleatória
 	if (rack.size() == 0)
 	{
 		char letter;
@@ -44,8 +44,8 @@ void setBag(ifstream& extractFile, vector<char>& bag, vector <char> rack)
 		srand(time(NULL)); // basta fazer uma vez
 	}
 
-	 Das vezes seguintes o que tem de entrar é a bag já com as letras que foram tiradas da rack, para ser só preciso dar shuffle. 
-	 Para isso, é preciso trocar as letras da rack com as últimas da bag quando se dá "refresh" na rack
+	 //Das vezes seguintes o que tem de entrar é a bag já com as letras que foram tiradas da rack, para ser só preciso dar shuffle. 
+	 //Para isso, é preciso trocar as letras da rack com as últimas da bag quando se dá "refresh" na rack
 
     random_shuffle(bag.begin(), bag.end());
 
@@ -73,7 +73,7 @@ int main(){
 		exit(1);
 	}
 
-	// Work in progress...
+	//Work in progress...
 	extractFile.ignore(14);
 	extractFile >> winnerPoints;
 	/*cout << winnerPoints << endl;*/
@@ -109,93 +109,93 @@ int main(){
 }
 
 
-// //alguns já devem estar no ficheiro fundox.cpp
-//#include <iostream>
-//#include <vector>
-//#include <fstream>
-//#include <string>
-//#include <algorithm>
-//
-//using namespace std;
-//
-//void bubbleSort(vector<char>& v)
-//{
-//	int length = v.size();
-//	bool stop;
-//
-//	while (length > 1)
-//	{
-//		stop = true;
-//
-//		for (int j = 0; j < length - 1; j++)
-//			if (v.at(j) > v.at(j + 1))
-//			{
-//				iter_swap(v.begin() + j, v.begin() + j + 1);
-//				stop = false;
-//			}
-//
-//		if (stop)
-//			break;
-//
-//		length--;
-//	}
-//}
-//
-//void setBag(ifstream& extractFile, vector<char>& bag, vector <char> rack)
-//{
-//	// Da primeira vez a função lê o ficheiro e põe as letras numa ordem aleatória
-//	if (rack.size() == 0)
-//	{
-//		char letter;
-//		int num_occurences;
-//
-//		while (extractFile >> letter >> num_occurences)
-//			for (int i = 1; i <= num_occurences; i++)
-//				bag.push_back(letter);
-//
-//		srand(time(NULL)); // basta fazer uma vez
-//	}
-//
-//	// Das vezes seguintes o que tem de entrar é a bag já com as letras que foram tiradas da rack, para ser só preciso dar shuffle. 
-//	// Para isso, é preciso trocar as letras da rack com as últimas da bag quando se dá "refresh" na rack
-//
-//	random_shuffle(bag.begin(), bag.end());
-//}
-//
-//int main()
-//{
-// // alguns destes não vão ser precisos porque já vão estar no main
-//	vector <char> bag, rack;
-//	ifstream extractFile("CONFIG.txt");
-//	string trash, wordFile;
-//	int winnerPoints;
-//
-//	if (!extractFile.is_open())
-//	{
-//		cout << "File CONFIG.txt not found!" << endl;
-//		exit(1);
-//	}
-//
-//	// Work in progress... Preciso de discutir isto convosco
-//	extractFile.ignore(14);
-//	extractFile >> winnerPoints;
-//	/*cout << winnerPoints << endl;*/
-//	extractFile.ignore(13);
-//	getline(extractFile, wordFile);
-//	/*cout << wordFile << endl;*/
-//	getline(extractFile, trash);
-//
-//	setBag(extractFile, bag, rack);
-//
-//	cout << endl;
-//
-//	extractFile.close();
-//
-//	// aqui algures define-se a primeira rack (rack = tal e coiso) e enquanto eles jogam a bag vai sofrendo resize
-//
-//	bubbleSort(rack);
-//
-//	// fazer setBag(extractFile, bag, rack) quando todos passam uma vez SÓ DEPOIS de dar reset à rack, TROCANDO os seus elementos com elementos da bag
-//
-//	return (0);
-//}
+ //alguns já devem estar no ficheiro fundox.cpp
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+void bubbleSort(vector<char>& v)
+{
+	int length = v.size();
+	bool stop;
+
+	while (length > 1)
+	{
+		stop = true;
+
+		for (int j = 0; j < length - 1; j++)
+			if (v.at(j) > v.at(j + 1))
+			{
+				iter_swap(v.begin() + j, v.begin() + j + 1);
+				stop = false;
+			}
+
+		if (stop)
+			break;
+
+		length--;
+	}
+}
+
+void setBag(ifstream& extractFile, vector<char>& bag, vector <char> rack)
+{
+	// Da primeira vez a função lê o ficheiro e põe as letras numa ordem aleatória
+	if (rack.size() == 0)
+	{
+		char letter;
+		int num_occurences;
+
+		while (extractFile >> letter >> num_occurences)
+			for (int i = 1; i <= num_occurences; i++)
+				bag.push_back(letter);
+
+		srand(time(NULL)); // basta fazer uma vez
+	}
+
+	// Das vezes seguintes o que tem de entrar é a bag já com as letras que foram tiradas da rack, para ser só preciso dar shuffle. 
+	// Para isso, é preciso trocar as letras da rack com as últimas da bag quando se dá "refresh" na rack
+
+	random_shuffle(bag.begin(), bag.end());
+}
+
+int main()
+{
+ // alguns destes não vão ser precisos porque já vão estar no main
+	vector <char> bag, rack;
+	ifstream extractFile("CONFIG.txt");
+	string trash, wordFile;
+	int winnerPoints;
+
+	if (!extractFile.is_open())
+	{
+		cout << "File CONFIG.txt not found!" << endl;
+		exit(1);
+	}
+
+	// Work in progress... Preciso de discutir isto convosco
+	extractFile.ignore(14);
+	extractFile >> winnerPoints;
+	/*cout << winnerPoints << endl;*/
+	extractFile.ignore(13);
+	getline(extractFile, wordFile);
+	/*cout << wordFile << endl;*/
+	getline(extractFile, trash);
+
+	setBag(extractFile, bag, rack);
+
+	cout << endl;
+
+	extractFile.close();
+
+	// aqui algures define-se a primeira rack (rack = tal e coiso) e enquanto eles jogam a bag vai sofrendo resize
+
+	bubbleSort(rack);
+
+	// fazer setBag(extractFile, bag, rack) quando todos passam uma vez SÓ DEPOIS de dar reset à rack, TROCANDO os seus elementos com elementos da bag
+
+	return (0);
+}
